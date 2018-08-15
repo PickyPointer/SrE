e4c8f06832da362862d375b8c71e8e92b73cde74
srgang <srgang@yeelmo.colorado.edu>
2018-08-15 15:55:25 -0600

    update to current_controller devices for interface name

--- labrad_tools/current_controller/devices/tc.py
+++ labrad_tools/current_controller/devices/tc.py
@@ -3,7 +3,7 @@ from devices.ldc80xx.ldc80xxSerial import Ldc80xxSerial
 class BlueTC(Ldc80xxSerial):
     autostart = False
     serial_server_name = 'yeelmo_serial'
-    serial_address = 'ttyUSBblue_controller'
+    serial_address = '/dev/ttyUSBblue_controller'
 
     pro8_slot = 5
     default_current = 0.150
